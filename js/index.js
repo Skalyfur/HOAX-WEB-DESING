7e8bbde4c34f6868ab11e1ea124362cc1744400d
Skallyfur <felipeloaizarios@gmail.com>
2024-12-01 20:29:58 -0500

    primer commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+
+    sr.reveal(".header-container", {
+        duration: 2000,
+        origin: "buttom",
+        distance: "-100px"
+    })
+
+    sr.reveal(".portada-text", {
+        duration: 2000,
+        origin: "right",
+        distance: "-100px"
+    })
+
+    sr.reveal(".boton-portada", {
+        duration: 2000,
+        origin: "right",
+        distance: "-100px"
+    })
+
+
+ScrollReveal().reveal('.trabajos-collage-imagenes', { scale: 0.85 });
+
+
+
+
+
